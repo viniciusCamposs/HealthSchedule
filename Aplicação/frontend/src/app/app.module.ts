5f5d4e6273824b1fef8a982d7f946abe018624ab
Vinicius Campos <camposvinicius560@gmail.com>
2022-03-02 21:06:47 -0300

    Criação do spinner

--- Aplicação/frontend/src/app/app.module.ts
+++ Aplicação/frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,7 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpInterceptorService } from './core/authentication/http-interceptor.service';
 import { LoginModule } from './pages/login/login.module';
+import { AppMaterialModule } from './shared/modules/app-material.module';
 import { SharedModule } from './shared/shared.module';
 
 
@@ -19,10 +22,13 @@ import { SharedModule } from './shared/shared.module';
     LoginModule,
     ReactiveFormsModule,
     FormsModule,
+    AppMaterialModule,
     SharedModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
