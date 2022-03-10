36f09d9d98c647105acbb51ee31fac61ed31a34d
Vinicius Campos <camposvinicius560@gmail.com>
2022-03-10 20:59:39 -0300

    Finalizei a search bar

--- Aplicação/frontend/src/app/shared/modules/app-material.module.ts
+++ Aplicação/frontend/src/app/shared/modules/app-material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,7 +30,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatToolbarModule,
     MatDividerModule,
     MatCardModule,
+    MatCheckboxModule,
     MatMenuModule,
+
     ReactiveFormsModule
   ]
 })
